377f675a695e1479bead6549e4d401562bb0a27d
vladislav2251 <vladdidyk1985@gmail.com>
2024-05-07 11:55:30 +0300

    add firebase

---
+++ frontend/src/components/ui/button.vue
@@ -0,0 +1,33 @@
+<template>
+        <button
+                v-if="type === 'button' || type === 'submit'"
+                :type="type"
+                :class="[ background ? 'before:absolute before:-start-3 before:top-0 before:-z-10 before:h-full before:w-[calc(100%+1.5rem)] before:origin-left before:-skew-x-[16deg] before:scale-x-0 before:transition-transform before:duration-500 hover:[&:not(:disabled)]:before:scale-x-100 focus-visible:[&:not(:disabled)]:before:scale-x-100 active:[&:not(:disabled)]:scale-[1.01] disabled:opacity-70 border-[#fffef9] px-6 py-3 md:px-8 md:py-4 md:text-lg hover:[&:not(:disabled)]:tracking-[0.075em] text-[#6919ff] before:bg-[#060918] bg-[#fffef9] hover:[&:not(:disabled)]:text-[#fffef9] focus-visible:text-[#fffef9]' : 'before:absolute before:-start-3 before:top-0 before:-z-10 before:h-full before:w-[calc(100%+1.5rem)] before:origin-left before:-skew-x-[16deg] before:scale-x-0 before:transition-transform before:duration-500 hover:[&:not(:disabled)]:before:scale-x-100 focus-visible:[&:not(:disabled)]:before:scale-x-100  active:[&:not(:disabled)]:scale-[1.01] disabled:opacity-70 border-[#fffef9] px-6 py-3 md:px-8 md:py-4 md:text-lg hover:[&:not(:disabled)]:tracking-[0.075em]  hover:[&:not(:disabled)]:text-[#6919ff] focus-visible:text-[#6919ff] text-[#fffef9] before:bg-[#fffef9]']"
+                class="relative isolate flex justify-center items-center select-none overflow-hidden rounded-full border align-middle transition-all duration-500"
+        >
+
+                <slot />
+        </button>
+
+        <router-link 
+                v-else
+                :to="to"
+                :class="[ background ? 'before:absolute before:-start-3 before:top-0 before:-z-10 before:h-full before:w-[calc(100%+1.5rem)] before:origin-left before:-skew-x-[16deg] before:scale-x-0 before:transition-transform before:duration-500 hover:[&:not(:disabled)]:before:scale-x-100 focus-visible:[&:not(:disabled)]:before:scale-x-100 active:[&:not(:disabled)]:scale-[1.01] disabled:opacity-70 border-[#fffef9] px-6 py-3 md:px-8 md:py-4 md:text-lg hover:[&:not(:disabled)]:tracking-[0.075em] text-[#6919ff] before:bg-[#060918] bg-[#fffef9] hover:[&:not(:disabled)]:text-[#6919ff] focus-visible:text-[#6919ff]' : 'before:absolute before:-start-3 before:top-0 before:-z-10 before:h-full before:w-[calc(100%+1.5rem)] before:origin-left before:-skew-x-[16deg] before:scale-x-0 before:transition-transform before:duration-500 hover:[&:not(:disabled)]:before:scale-x-100 focus-visible:[&:not(:disabled)]:before:scale-x-100  active:[&:not(:disabled)]:scale-[1.01] disabled:opacity-70 border-[#fffef9] px-6 py-3 md:px-8 md:py-4 md:text-lg hover:[&:not(:disabled)]:tracking-[0.075em] hover:[&:not(:disabled)]:text-[#6919ff] focus-visible:text-[#6919ff] text-[#fffef9] before:bg-[#fffef9]']"
+                class="relative isolate flex justify-center items-center select-none overflow-hidden rounded-sm border align-middle transition-all duration-500"
+        >
+
+                <slot />
+        </router-link>
+</template>
+
+<script setup lang="ts">
+
+        interface IProps {
+                type: string
+                to: string;
+                background: boolean;
+        };
+        
+        const props = defineProps<IProps>();
+
+</script>
